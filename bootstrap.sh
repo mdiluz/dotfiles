7472acee2c936973ab5536304c756573d463b09e
Marc Di Luzio <mdiluzio@feralinteractive.com>
2014-11-19 10:40:30 +0000

    Have bootstrap output a .dotfilesdir file to be used to locate dotfiles installation location

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s $DIR/bin ~/bin
+echo "DOTFILESDIR=$DIR" > ~/.dotfilesdir
+
+# make the tmp directory
+mkdir $DIR/tmp
+
 ln -s $DIR/bashrc ~/.bashrc
 ln -s $DIR/bash_aliases ~/.bash_aliases
