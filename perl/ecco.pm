02d9bd4c86cdd4d922d814862df058c6694c425e
Marc Di Luzio <mdiluzio@feralinteractive.com>
2014-11-19 10:14:16 +0000

    Add new dotfiles test scripts
    Add error, success, warning and testing functions to ecco.pm

--- perl/ecco.pm
+++ perl/ecco.pm
@@ -7,16 +7,43 @@ use warnings;
 
 # Export functions
 use Exporter qw(import);
-our @EXPORT_OK = qw(ecco);
+our @EXPORT_OK = qw(ecco eccow eccoe eccos ecco_test);
 
 # Needed features
 use Term::ANSIColor ':constants';
 use feature 'say';
 
-# Simply echo the given string
+# simply echo the given string
 sub ecco
 {
-	say @_;
+	say BLUE, "@_", RESET;
+}
+
+# echo given warning
+sub eccow
+{
+	say YELLOW, "WARNING: @_", RESET;
+}
+
+# echo an error
+sub eccoe
+{
+	say RED, "ERROR: @_", RESET;
+}
+
+# echo a success
+sub eccos
+{
+	say GREEN, "SUCCESS: @_", RESET;
+}
+
+# test the ecco module
+sub ecco_test
+{
+	ecco "BLUE";
+	eccow "YELLOW";
+	eccoe "RED";
+	eccos "GREEN";
 }
 
 1; # True statement
