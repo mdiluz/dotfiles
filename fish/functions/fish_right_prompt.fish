c6f601da527d3c6c9f6268e9466065a9b1da5ce0
Marc Di Luzio <mdiluzio@feralinteractive.com>
2016-06-07 11:35:49 +0100

    Pull in fish_right_prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,1 +1,11 @@
-/feraldev/src/trunk/Companies/Feral/Development/Resources/Scripts/Users/mdiluzio/fish/functions/fish_right_prompt.fish
+function fish_right_prompt -d "Write out the right prompt"
+
+	set -l __last_status $status
+
+	set_color blue
+	if test $__last_status -gt 0
+		set_color red
+	end
+	echo -n "$CMD_DURATION" ms
+	set_color normal
+end
