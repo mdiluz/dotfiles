e32dbcccc7cbb9f39f04b74b7d570c431741391a
Marc Di Luzio <mdiluzio@feralinteractive.com>
2016-06-07 11:23:31 +0100

    Update fish_prompt
    	> Echo part by part
    	> Only output username as needed
    	> remove status output in favor of right side prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,26 +1,29 @@
 function fish_prompt --description 'Write out the prompt'
 
-	set stat $status
-
 	# Just calculate these once, to save a few cycles when displaying the prompt
 	if not set -q __fish_prompt_hostname
 		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
 	end
 
-	#Set the color for the status depending on the value
-	set __fish_color_status (set_color -o green)
-	if test $stat -gt 0
-		set __fish_color_status (set_color -o red)
-	end
-
 	switch $USER
 	case root
-		set __fish_prompt (printf '%s@%s:%s# ' $USER $__fish_prompt_hostname "%s" )
+		echo -n $USER"@" 
+		echo -n $__fish_prompt_hostname
 	case '*'
-		set __fish_prompt (printf '%s[%s]%s%s@%s:%s%s %s(%s)%s \f\r> ' (set_color yellow) (date "+%H:%M:%S") (set_color -o blue) $USER $__fish_prompt_hostname (set_color green) "%s" "$__fish_color_status" "$stat" (set_color normal))
+		# Print the current time in HH:MM:SS format
+		set_color yellow 
+		echo -n "["(date +%H:%M:%S)"]" 
+		
+		# Print the current user and hostname if needed
+		set_color -o blue 
+		if test $USER != $LOGNAME
+			echo -n $USER"@"
+		end 
+		echo -n $__fish_prompt_hostname 
 	end
 
-	# shorter prompt
+
+	# cwd part of prompt
 	set __prompt_cwd_len (pwd | wc -c)
 	set __prompt_command_len (echo $__fish_prompt | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | wc -m)
 	set __prompt_total_len (math "$__prompt_cwd_len+$__prompt_command_len")
@@ -30,5 +33,9 @@ function fish_prompt --description 'Write out the prompt'
 		set __prompt_cwd (pwd)
 	end
 
-	printf "$__fish_prompt" "$__prompt_cwd"
+	set_color green
+	echo -n ":"
+	echo $__prompt_cwd
+	set_color normal
+	echo "> "
 end
