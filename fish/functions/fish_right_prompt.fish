a8b0c0bacc1f92cd7321437b81149a2f09edf40e
Marc Di Luzio <mdiluzio@feralinteractive.com>
2016-06-09 13:35:00 +0100

    Print the last command as well in the right side prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -6,6 +6,15 @@ function fish_right_prompt -d "Write out the right prompt"
 	if test $__last_status -gt 0
 		set_color red
 	end
-	echo -n "$__time"
+
+	# print last command
+	set __last_cmd $history[1]
+	# Limit to max length
+	set __length_last 20
+	if [ (string length $__last_cmd) -gt $__length_last ];
+		set __last_cmd ( printf "%s…" ( string sub --length $__length_last $__last_cmd ) )
+	end
+
+	printf "[%s] %s" $__last_cmd $__time
 	set_color normal
 end
