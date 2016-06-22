556e58a77790f0a6e978f2a89afad04c5ee0b519
Marc Di Luzio <mdiluzio@feralinteractive.com>
2016-06-22 08:54:06 +0100

    Update my prompt code somewhat
    	Use some nice unicodes and colours
    	refactor the code so it's a little more readable

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -5,27 +5,26 @@ function fish_prompt --description 'Write out the prompt'
 		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
 	end
 
+	# Grab hostname and user
+	set -l __prompt_user_host ""	
 	switch $USER
 	case root
-		echo -n $USER"@" 
-		echo -n $__fish_prompt_hostname
+		set __prompt_user_host "root@"$__fish_prompt_hostname
 	case '*'
-		# Print the current time in HH:MM:SS format
-		set_color yellow 
-		echo -n "["(date +%H:%M:%S)"]" 
-		
-		# Print the current user and hostname if needed
-		set_color -o blue 
+		# Only use the current user if needed
 		if test $USER != $LOGNAME
-			echo -n $USER"@"
-		end 
-		echo -n $__fish_prompt_hostname 
+			set __prompt_user_host $USER"@"$__fish_prompt_hostname
+		else
+			set __prompt_user_host $__fish_prompt_hostname
+		end
 	end
 
+	# Construct a base prompt with space for colors
+	set __prompt_base "%s╔═ %s"(date +%H:%M:%S)" %s"$__prompt_user_host
 
-	# cwd part of prompt
+	# Sort out the CWD shortened if needed 
 	set __prompt_cwd_len (pwd | wc -c)
-	set __prompt_command_len (echo $__fish_prompt | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | wc -m)
+	set __prompt_command_len (echo $__prompt_base | sed -r "s/%s//g" | wc -m)
 	set __prompt_total_len (math "$__prompt_cwd_len+$__prompt_command_len")
 	if math "$__prompt_total_len>$COLUMNS" > /dev/null
 		set __prompt_cwd (prompt_pwd)
@@ -33,9 +32,19 @@ function fish_prompt --description 'Write out the prompt'
 		set __prompt_cwd (pwd)
 	end
 
-	set_color green
-	echo -n ":"
-	echo $__prompt_cwd
-	set_color normal
-	echo "> "
+	# Assemble our top and bottom lines
+	set __prompt_full_top $__prompt_base"%s:%s"$__prompt_cwd
+	set __prompt_bottom_line "%s╚═%s "
+
+	# Colorise the output
+	printf $__prompt_full_top (set_color blue) \
+	                          (set_color yellow) \
+	                          (set_color -o blue) \
+	                          (set_color 444444) \
+	                          (set_color green)
+	# newline
+	echo ""
+	printf $__prompt_bottom_line (set_color blue) \
+	                             (set_color normal)
+
 end
