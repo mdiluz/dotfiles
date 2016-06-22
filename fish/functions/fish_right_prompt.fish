b8d094de4ab4f3df69353af02b0128b3e6cf7aaa
Marc Di Luzio <mdiluzio@feralinteractive.com>
2016-06-22 08:55:37 +0100

    Update my right_prompt to be neater with shorter commands and coloured output

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,20 +1,26 @@
 function fish_right_prompt -d "Write out the right prompt"
 
 	set -l __last_status $status
-	set -l __time (__fish_ms_to_human $CMD_DURATION)
-	set_color blue
-	if test $__last_status -gt 0
-		set_color red
-	end
+	set -l __time ( __fish_ms_to_human $CMD_DURATION )
+	set -l __last_cmd $history[1]
 
-	# print last command
-	set __last_cmd $history[1]
+	# format the last command
+	# Remove a trailing space
+	set __last_cmd ( string replace -r " \$" "" $__last_cmd)
 	# Limit to max length
 	set __length_last 20
 	if [ (string length $__last_cmd) -gt $__length_last ];
 		set __last_cmd ( printf "%s…" ( string sub --length $__length_last $__last_cmd ) )
 	end
 
-	printf "[%s] %s" $__last_cmd $__time
+	# Use dark grey unless the last command failed
+	set_color 444444
+	if test $__last_status -gt 0
+		set_color -o red
+	end
+	printf "[%s] " $__last_cmd
+	if [ $__time ]
+		printf "%s " $__time
+	end 
 	set_color normal
 end
