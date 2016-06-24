327e0554bb7b3d56d3c3d661752c4602cf0b41ce
Marc Di Luzio <mdiluzio@feralinteractive.com>
2016-06-24 15:15:06 +0100

    update basic config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,17 @@ set -g MANPAGER most
 if test -f /etc/profile.d/autojump.fish
 	source /etc/profile.d/autojump.fish
 end
+
+# Notify on extra long functions
+function --on-event fish_postexec __notify_long_command
+    if math "$CMD_DURATION > 5000" > /dev/null
+        set -l __last_cmd $argv
+        if type -q notify-send
+            notify-send "cmd done" "$__last_cmd"
+        else if type -q osascript
+            set -l __last_cmd ( echo $__last_cmd | tr -d \"\' )
+            osascript -e "display notification \"$__last_cmd\" with title \"cmd done\""
+        end 
+    end 
+end
+
