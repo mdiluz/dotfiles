7f8bdc30c178a29c191fdbc8bb4d12f6e9f82fda
Marc Di Luzio <mdiluzio@feralinteractive.com>
2017-10-26 15:28:56 +0100

    Allow for multiline last commands

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -2,17 +2,17 @@ function fish_right_prompt -d "Write out the right prompt"
 
 	set -l __last_status $status
 	set -l __time ( __fish_ms_to_human $CMD_DURATION )
-	set -l __last_cmd $history[1]
+	set -l __last_cmd "$history[1]"
 
 	# test for string
 	if type -q string 
 		# format the last command
 		# Remove a trailing space
-		set __last_cmd ( string replace -r " \$" "" $__last_cmd)
+		set __last_cmd ( string replace -r " \$" "" "$__last_cmd" )
 		# Limit to max length
 		set __length_last 20
-		if [ (string length $__last_cmd) -gt $__length_last ];
-			set __last_cmd ( printf "%s…" ( string sub --length $__length_last $__last_cmd ) )
+		if [ (string length "$__last_cmd") -gt $__length_last ];
+			set __last_cmd ( printf "%s…" ( string sub --length $__length_last "$__last_cmd" ) )
 		end
 	end
 
@@ -21,7 +21,7 @@ function fish_right_prompt -d "Write out the right prompt"
 	if test $__last_status -gt 0
 		set_color -o red
 	end
-	printf "[%s] " $__last_cmd
+	printf "[%s] " "$__last_cmd"
 	if [ $__time ]
 		printf "%s " $__time
 	end 
