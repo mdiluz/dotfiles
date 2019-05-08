d7f6e8e0dd4bbfbda10f68b55e424b675618019d
Marc Di Luzio <marcdi@unity3d.com>
2019-05-08 16:31:38 +0100

    Add git lg

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,3 +27,6 @@ do
 	mkdir -p "$FISHDIR/$FISHNAME"
 	ln -s "$DIR/fish/$FISHNAME/"* "$FISHDIR/$FISHNAME"
 done < <( find "$DIR/fish/" -maxdepth 1 -mindepth 1 -type d -print0 )
+
+# Ensure we have git lg
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
