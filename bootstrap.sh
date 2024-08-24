92b54fe306ef9c1ccd40d43d7b67e7636ede8273
Marc Di Luzio <marc.diluzio@gmail.com>
2024-08-24 23:30:48 +0100

    Remove alacrytty and broken fish link

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,13 +17,10 @@ fi
 echo "Linking configs"
 ln -s "$DIR/config/vimrc" ~/.vimrc
 ln -s "$DIR/config/tmux.conf" ~/.tmux.conf
-mkdir -p "~/.config/alacritty/"
-ln -s "$DIR/config/alacrity.yml" ~/.config/
 
 # Linking fish
 echo "Linking fish"
 FISHDIR=~/.config/fish/
-ln -s "$DIR/fish/config.fish" $FISHDIR
 while IFS= read -r -d '' FISHES
 do
 	FISHNAME=$( basename "$FISHES" )
